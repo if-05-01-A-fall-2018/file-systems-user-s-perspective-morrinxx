617071b42ea38553637c3f1552df9346fc7b1bf4
Marcel <marcel.poelzl@hotmail.com>
2019-03-19 12:40:16 +0100

    first commit

---
+++ cpf.c
@@ -0,0 +1,9 @@
+ #include <sys/stat.h>
+
+ struct stat attributes;
+ int main(int argc, char const *argv[]) {
+   if(argc == 0 || argc == 1)return 0;
+   const char* originPath = argv[0];
+   const char* 
+   return 0;
+ }
