05081b213d39ba9c9a7a75d9e1324912bb17671d
marcel <>
2019-03-22 15:30:36 +0100

    cpf.c finished

--- cpf.c
+++ cpf.c
@@ -11,21 +11,23 @@
    const char* newPath = argv[2];
    int fd = open(originPath, O_RDONLY);
    if(fd < 0){
-       printf("Error when opening the file: \n" + fd);
+       printf("Error when opening the file: \n");
        return 0;
    }
-   printf("File-open: SUCCESS!\n");
+   printf("File-open: SUCCESS! \n");
    int sizeOfFile = lseek(fd, 0, SEEK_END);
    char* buffer[sizeOfFile];
+   lseek(fd, 0, SEEK_SET);
    read(fd, buffer, sizeof(buffer));
    int fd2 = open(newPath, O_WRONLY);
-   if(fd2 != 0){
-       mkdir(newPath, O_WRONLY);
-       fd2 = open(newPath, O_WRONLY);
-   }
-   int success = write(fd, buffer, sizeof(buffer));
 
-   if(success == 0)printf("Successfully copied");
+   if(fd2 < 0){
+       fd2 = open(newPath, O_CREAT | O_WRONLY);
+   }
+   else printf("Owerwriting file \n");
+   int success = write(fd2, buffer, sizeOfFile);
+
+   if(success == sizeOfFile)printf("Successfully copied \n");
    close(fd);
    close(fd2);
    return 0;
